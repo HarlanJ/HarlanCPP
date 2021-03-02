1025a55eed921d56f1030e6d986dfaaea61195d6
Harlanj <john@shipritesoftware.com>
2021-03-01 21:48:51 -0500

    Added a Range struct and function to use it

--- HarlanCommon.hpp
+++ HarlanCommon.hpp
@@ -1,6 +1,8 @@
 #ifndef HARLANCOMMON
 #define HARLANCOMMON
 
+#include <stdlib.h>
+
 namespace Harlan{
 	const double PI 		= 3.14159265358979323846;
 	const double TWO_PI = 6.28318530717958647692;
@@ -19,6 +21,19 @@ namespace Harlan{
 		} else {
 			return (b-a) <= tol;
 		}
+	}
+
+	struct Range{
+		double bottom;
+		double top;
+	};
+
+	bool inRange(double val, Range r){
+		return (val > r.bottom && val < r.top);
+	}
+
+	double getRandInRange(Range r){
+		return std::rand() / (RAND_MAX / (r.top-r.bottom));
 	}
 }
 
