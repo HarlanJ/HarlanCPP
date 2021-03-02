6a1cd073ceaf66a7cc9683bab5dfa0e236f5b60b
Harlanj <john@shipritesoftware.com>
2021-03-01 23:21:24 -0500

    Created baseline for Vector2 class

---
+++ Vector2.hpp
@@ -0,0 +1,37 @@
+#ifndef VECTOR2_HPP
+#define VECTOR2_HPP
+
+#include <math.h>
+
+class Vector2{
+  double x, y;
+  Vector2(){
+    x = 0;
+    y = 0;
+  }
+
+  Vector2(double x, double y){
+    this->x = x;
+    this->y = y;
+  }
+
+  double mag(){
+    return sqrt(x*x + y*y);
+  }
+
+  // Radians
+  void rotate(double theta){
+    x = x*cos(theta) - y*sin(theta);
+    y = y*cos(theta) + x*sin(theta);
+  }
+
+  static Vector2 rotate(Vector2 v, double theta){
+    return Vector2(
+      v.x*cos(theta) - v.y*sin(theta),
+      v.y*cos(theta) + v.x*sin(theta)
+    );
+  }
+
+};
+
+#endif
