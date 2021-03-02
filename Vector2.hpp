62f7a3c57c365947ec19b8698b70bbfaf40e2eb9
Harlanj <john@shipritesoftware.com>
2021-03-01 23:38:56 -0500

    Whoops, nothing in Vector2 was public

--- Vector2.hpp
+++ Vector2.hpp
@@ -4,34 +4,34 @@
 #include <math.h>
 
 class Vector2{
-  double x, y;
-  Vector2(){
-    x = 0;
-    y = 0;
-  }
+  public:
+    double x, y;
+    Vector2(){
+      x = 0;
+      y = 0;
+    }
 
-  Vector2(double x, double y){
-    this->x = x;
-    this->y = y;
-  }
+    Vector2(double x, double y){
+      this->x = x;
+      this->y = y;
+    }
 
-  double mag(){
-    return sqrt(x*x + y*y);
-  }
+    double mag(){
+      return sqrt(x*x + y*y);
+    }
 
-  // Radians
-  void rotate(double theta){
-    x = x*cos(theta) - y*sin(theta);
-    y = y*cos(theta) + x*sin(theta);
-  }
+    // Radians
+    void rotate(double theta){
+      x = x*cos(theta) - y*sin(theta);
+      y = y*cos(theta) + x*sin(theta);
+    }
 
-  static Vector2 rotate(Vector2 v, double theta){
-    return Vector2(
-      v.x*cos(theta) - v.y*sin(theta),
-      v.y*cos(theta) + v.x*sin(theta)
-    );
-  }
-
+    static Vector2 rotate(Vector2 v, double theta){
+      return Vector2(
+        v.x*cos(theta) - v.y*sin(theta),
+        v.y*cos(theta) + v.x*sin(theta)
+      );
+    }
 };
 
 #endif
